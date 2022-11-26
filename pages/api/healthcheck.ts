02877d5cff95e52b37c2fa6c0af4aaee224988d8
Deric Cain <deric@shipt.com>
2022-11-26 05:55:58 +0000

    set cache on

--- pages/api/healthcheck.ts
+++ pages/api/healthcheck.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') return res.status(200).send({ message: 'healthy' });
+  if (req.method === 'GET') {
+    res.setHeader('Cache-Control', 's-maxage=610');
+    return res.status(200).send({ message: 'healthy' });
+  }
   return res.status(404).send({ message: 'Not found.' });
 }
